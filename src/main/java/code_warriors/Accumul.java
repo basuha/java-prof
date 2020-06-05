ea00c7ee4c675646c9b4537b0f6134b42a7d63ed
basuha <bassturbatorark@gmail.com>
2020-06-05 11:28:27 +0600

    initial commit

---
+++ src/main/java/code_warriors/Accumul.java
@@ -0,0 +1,47 @@
+package code_warriors;
+import static org.junit.Assert.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+
+/**
+ * This time no story, no theory. The examples below show you how to write function accum:
+ *
+ * Examples:
+ *
+ * accum("abcd") -> "A-Bb-Ccc-Dddd"
+ * accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+ * accum("cwAt") -> "C-Ww-Aaa-Tttt"
+ */
+public class Accumul {
+    public static String accum(String s) {
+        StringBuilder out = new StringBuilder();
+        for (int i = 1; i < s.length(); i++) {
+            out.append(StringUtils.capitalize(String.valueOf(s.charAt(i - 1)).repeat(i)))
+                    .append("-");
+        }
+        out.deleteCharAt(out.lastIndexOf("-"));
+        return out.toString();
+    }
+}
+
+class AccumulTest {
+
+    public static void main(String[] args) {
+        test();
+        System.out.println(Accumul.accum("ZpglnRxqenU"));
+    }
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public static void test() {
+        System.out.println("Fixed Tests accum");
+        testing(Accumul.accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        testing(Accumul.accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        testing(Accumul.accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        testing(Accumul.accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        testing(Accumul.accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+}
---
+++ src/main/java/code_warriors/HelloWorld.java
@@ -0,0 +1,60 @@
+package code_warriors;
+
+import org.apache.commons.lang3.StringUtils;
+import java.util.Arrays;
+
+public class HelloWorld {
+    static String toCamelCase(String s){
+        if (s.isEmpty())
+            return s;
+        String splitSymbol = s.contains("-") ? "-" : "_";
+        String[] strings = s.split(splitSymbol);
+        boolean capitalized = StringUtils.isAllUpperCase(s.substring(0,1));
+        boolean isFirst = true;
+        StringBuilder sb = new StringBuilder();
+        for (String string : strings) {
+            sb.append(capitalized ? StringUtils.capitalize(string)
+                    : (isFirst ? string : StringUtils.capitalize(string)));
+            isFirst = false;
+        }
+        return sb.toString();
+    }
+}
+
+
+class FindOdd {
+    public static int findIt(int[] a) {
+        Arrays.sort(a);
+
+        int counter = 0;
+        int odd = 0;
+        int biggestNumber = a[a.length - 1];
+        boolean isBiggerThanOne = a.length > 1;
+
+        for (int i = 0; i < (isBiggerThanOne ? a.length - 1 : a.length); i++) {
+            if (counter == 0) {
+                counter++;
+            }
+            if (a[i] == a[isBiggerThanOne ? i + 1 : i] && isBiggerThanOne) {
+                counter++;
+            } else {
+                if (counter % 2 != 0) {
+                    odd = a[i];
+                } else if (counter == biggestNumber) {
+                    odd = a[i+1];
+                }
+                counter = 0;
+            }
+        }
+
+        return odd;
+    }
+}
+
+class XO {
+    public static boolean getXO (String str) {
+        return StringUtils.countMatches(str.toLowerCase(), "x")
+                == StringUtils.countMatches(str.toLowerCase(), "o");
+    }
+}
+
