6c22446bd9ab30d614d5285d19ea00cf554221a7
basuha <bassturbatorark@gmail.com>
2020-06-23 14:13:58 +0600

    regexp complete

--- src/main/java/alishev/regexp/ReplaceAllEx.java
+++ src/main/java/alishev/regexp/ReplaceAllEx.java
@@ -4,5 +4,10 @@ public class ReplaceAllEx {
     public static void main(String[] args) {
         String a = "Hello           there hey";
         System.out.println(a.replaceAll(" +", "."));
+
+
+        String b = "Hello45435there5435hey";
+        System.out.println(b.replaceAll("\\d+", "w"));
+        System.out.println(b.replaceFirst("\\d+", "w"));
     }
 }
